83e6d332a5e42edd2fd5f1c633127412bf3a1c52
Tefan Haetami <tefanhaetami87@gmail.com>
2024-05-29 10:36:41 +0700

    chore: Update app module with database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { UtilsController } from './utils/utils.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      useUnifiedTopology: true,
+      entities: [User],
+      synchronize: true,
+      verboseRetryLog: true,
+      // dropSchema: true,
+    }),
+    UserModule,
+  ],
+  controllers: [UtilsController],
+  providers: [],
 })
 export class AppModule {}
