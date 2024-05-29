f8d96b1b1d2b48b2ac058c91bfb4abebcf444fe4
Tefan Haetami <tefanhaetami87@gmail.com>
2024-05-29 11:27:16 +0700

    feat: Add @ApiTags('utils') to UtilsController

--- src/utils/utils.controller.ts
+++ src/utils/utils.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { hash } from '../helpers';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('utils')
 @Controller('utils')
 export class UtilsController {
   @Get('text-hasher')
