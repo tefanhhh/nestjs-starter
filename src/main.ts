e0f14d74c171b9326270d7f4af84ffe11be083b3
Tefan Haetami <tefanhaetami87@gmail.com>
2024-05-29 10:40:55 +0700

    feat: Implement cluster mode for improved performance and scalability
    This commit adds cluster mode to the application, allowing it to take advantage of multiple CPU cores for improved performance and scalability. The primary process forks multiple worker processes, each handling incoming requests. The application is also updated to set a global prefix for the API, enable CORS, and generate Swagger documentation using the Nest JS Starter API. This change enhances the application's ability to handle high traffic loads and provides a better user experience.
    
    Refactor the main.ts file to implement cluster mode and update application settings for improved performance and scalability.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    cluster.on('exit', (worker) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    app.setGlobalPrefix('api');
+    app.enableCors();
+    const config = new DocumentBuilder()
+      .setTitle('Nest JS Starter API')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .addTag('NEST_JS_API')
+      .addSecurity('bearer', {
+        type: 'http',
+        scheme: 'bearer',
+      })
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('', app, document);
+    await app.listen(3000);
+  }
 }
 bootstrap();
